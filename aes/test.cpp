582d63b4e3f742877e2405bc8f32e7f5eb8a049d
goodpeanuts <143506992+goodpeanuts@users.noreply.github.com>
2023-12-12 16:26:22 +0000

    AES init

---
+++ aes/test.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: goodpeanuts 143506992+goodpeanuts@users.noreply.github.com
+ * @Date: 2023-12-12 15:29:02
+ * @LastEditors: goodpeanuts 143506992+goodpeanuts@users.noreply.github.com
+ * @LastEditTime: 2023-12-12 15:33:12
+ * @FilePath: /learning-cryptology/aes/test.cpp
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by goodpeanuts, All Rights Reserved. 
+ */
+#include<iostream>
+
+int main()
+{
+    unsigned char a = - 0x57;
+    char b = - 0x57;
+    printf("%x\n", a); 
+    printf("%x\n", b);
+}
